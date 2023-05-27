2575077a26c357fc0abd49161cb2f5b0028bff21
meliman23 <110848744+meliman23@users.noreply.github.com>
2023-05-27 16:38:24 -0500

    adding other days

--- chas-scheduel/chas-app/src/App.js
+++ chas-scheduel/chas-app/src/App.js
@@ -3,16 +3,44 @@ import Nav from './Components/Nav'
 import Card from "./Components/Card"
 
 import './App.css';
+//the adresses dont work yet 
 const bambiAdress = 'https://maps.apple.com/?address=6357%20NW%2063rd%20St%0AOklahoma%20City,%20OK%20%2073132%0AUnited%20States'
 const components = [
-  {day:'Sunday',
+  {day:'Monday',
    time:'8am - 10am',
    name:'Bambi',
    adress: bambiAdress,
   time2:'10:30am - 2:30pm',
 name2:'Marilyn',
-adress2:''}
-  ];
+adress2:''},
+ {day:'Tuesday',
+time:'8am - 11am',
+name:'Marilyn',
+adress:'',
+name2:'Cynthia',
+time2:'11:30am - 1:30pm',
+adress2:''},
+ {day:'Wednesday',
+time:'8am - 10am',
+name:'Bambi',
+adress:'',
+name2:'Cynthia',
+time2:'10:30am - 12:30pm',
+adress2:''}, 
+{day:'Thusday',
+time:'8am - 10am',
+name:'Cynthia',
+adress:'',
+}, 
+{day:'Friday',
+time:'8am - 1:30',
+name:'Marilyn',
+adress:''}, 
+{day:'Saturday',
+time:'9:30 - 12:30pm ',
+name:'coy',
+adress:'',
+}];
 
 const list = components.map((component, index) =>(
 <Card
